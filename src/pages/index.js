f89d09124f2c1d53f3c2b963b0e6cba0e2653c1c
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-07-14 15:55:05 -0400

    Init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,31 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.scss"
+import anime from 'animejs'
 
-const IndexPage = () => (
+
+
+const IndexPage = () => {
+  useEffect(() => {
+    anime({
+      targets: '.para',
+      rotate: '1turn',
+      backgroundColor: '#FFF',
+      duration: 8000
+    });
+    return () => {
+    }
+  }, [])
+  return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p class="para">Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
@@ -18,5 +33,6 @@ const IndexPage = () => (
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
+  }
 
 export default IndexPage
